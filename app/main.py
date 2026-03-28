bb726fd6f101f75381b9c2bbb98d1a2f7445b2e9
fcaguileraj7 <fcaguileraj7@gmail.com>
2026-03-28 18:03:07 -0500

    endpoint inicial de piezas para acrilico posterior

--- app/main.py
+++ app/main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Datos iniciales (MVP)
+piezas = [
+    {"id": 1, "nombre": "Cerradura imán", "estado": "pendiente"},
+    {"id": 2, "nombre": "Acople cerradura imán", "estado": "pendiente"},
+    {"id": 3, "nombre": "Posición delantera de puertas", "estado": "pendiente"},
+    {"id": 4, "nombre": "Posición trasera de puertas", "estado": "pendiente"},
+]
+
 @app.get("/")
 def inicio():
     return {"mensaje": "API funcionando correctamente"}
+
+@app.get("/piezas")
+def obtener_piezas():
+    return piezas
