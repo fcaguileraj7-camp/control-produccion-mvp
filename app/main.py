7283a8748b9b3f463ea8e9ac51d786918740877f
alberthsantiagoleal-cmyk <alberthsantiagoleal@gmail.com>
2026-04-09 01:39:52 -0500

    Arreglo main

--- app/main.py
+++ app/main.py
@@ -6,28 +6,3 @@ app = FastAPI()
 def inicio():
     return {"mensaje": "API funcionando correctamente"}
 
-from piezas import (
-    crear_tabla,
-    insertar_piezas_iniciales,
-    obtener_piezas,
-    actualizar_estado
-)
-
-# 1. Crear tabla
-crear_tabla()
-
-# 2. Insertar datos
-insertar_piezas_iniciales()
-
-# 3. Mostrar antes
-print("ANTES:")
-for pieza in obtener_piezas():
-    print(pieza)
-
-# 4. Actualizar estado
-actualizar_estado(1, "en_proceso")
-
-# 5. Mostrar después
-print("\nDESPUÉS:")
-for pieza in obtener_piezas():
-    print(pieza)
